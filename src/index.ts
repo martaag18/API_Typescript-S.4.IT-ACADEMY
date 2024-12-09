6ec4d1bc559cddfa1eaa44de521ab3988c7daa9f
Marta Altimir <martaag18@gmail.com>
2024-12-09 19:20:45 +0100

    Add dynamic navbar and date display using TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 
 import { JokeResponse } from "./interfaces";
 
-const $joke = document.querySelector(".jokes") as  HTMLParagraphElement;
+const $joke = document.getElementById("joke-text") as  HTMLParagraphElement;
 const $button = document.getElementById("btn-next") as HTMLButtonElement;
+const $dataNavBar = document.getElementById("current-date") as HTMLButtonElement;
 
+
+addEventListener("DOMContentLoaded", (e) => {
+    const today = new Date();
+    const formattedDateTime = today.toLocaleString("es-ES");
+    $dataNavBar.textContent = formattedDateTime;
+});
 
 $button.addEventListener("click", (e) => {
     e.preventDefault();
@@ -26,7 +33,7 @@ async function getJoke(): Promise<void> {
         }
 
         const json: JokeResponse = await resp.json();
-        $joke.textContent = json.joke; 
+        $joke.textContent = `${json.joke}`;
     } catch (err: any) {
         const message = err.statusText || "An error occurred";
         $joke.textContent = `Error ${err.status}: ${message}`;
